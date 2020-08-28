e3c5ea38e48feb6b7e9d5d3c65775bc6d4fae63a
Nikhil Kumar <developernik18@gmail.com>
2020-08-28 13:15:21 +0530

    Created Burger Builder component basic structure
    Created Burger Builder basic structure and used the component in App component. Further added css class for main element in Layout component.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import './Layout.css'
 
 const Layout = (porps) => (
   <Aux>
     <div>Toolbar , SideDrawer, Backdrop</div>
-    <main>
+    <main className="Content">
       {porps.children}
     </main>
   </Aux>
