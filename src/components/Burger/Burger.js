ad27105115a3b5b6a953bcd0ddac834a7fc08a9f
Nikhil Kumar <developernik18@gmail.com>
2020-08-31 12:30:21 +0530

    added css module for design and updated burger component
    Burger.js component got updated in this commit. Removed icon call from manifest.json and Updated css in module format to be used in scoped way.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,1 +1,18 @@
 import React from 'react';
+
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+  return (
+    <div className={classes.Burger}>
+      Just Check
+      <BurgerIngredient type="bread-top" />
+      <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="meat" />
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+}
+
+export default burger;
