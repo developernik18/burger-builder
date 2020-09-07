915ebf9f30bf454a56ff8f0e1c944ef26044574a
Nikhil Kumar <developernik18@gmail.com>
2020-09-07 20:12:35 +0530

    Add order now button
    Added Order Now button in the app which gets disabled if no ingredients are added and active when ingredients got selected

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,7 +20,19 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchasable: false
+  }
+
+  updatePurchasableState (ingredients) {
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      })
+      .reduce ((sum, el) => {
+        return sum + el;
+      } , 0);
+      this.setState({purchasable: sum > 0});
   }
 
   addIngredientHandler = (type) => {
@@ -34,7 +46,7 @@ class BurgerBuilder extends Component {
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice + priceAddition;
     this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
-
+    this.updatePurchasableState(updatedIngredients);
   }
 
   removeIngredientHandler = (type) => {
@@ -51,7 +63,7 @@ class BurgerBuilder extends Component {
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice - priceDeduction;
     this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
-
+    this.updatePurchasableState(updatedIngredients);
   }
 
   render () {
@@ -68,6 +80,7 @@ class BurgerBuilder extends Component {
         ingredientAdded={this.addIngredientHandler}
         ingredientRemoved={this.removeIngredientHandler}
         disabled={disabledInfo}
+        purchasable = {this.state.purchasable}
         price = {this.state.totalPrice} /></div>
       </Aux>
     );
