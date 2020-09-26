f8b919212c2ca8bba9801ba8a7ac02a072a0cdf5
Nikhil Kumar <developernik18@gmail.com>
2020-09-26 16:26:58 +0530

    Added toggle function on menu click

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,11 +3,12 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItem/NavigationItem';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div> MENU </div>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
     <div className={classes.Logo}>
         <Logo/>
     </div>
